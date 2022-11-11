74ca60771774b80026493073ac8a7be2b7dda303
Nhu <tran.hoang.nhu.1997@gmail.com>
2022-11-11 12:21:33 +0700

    update item card

--- js/profile.js
+++ js/profile.js
@@ -1,13 +1,24 @@
-var downContact = document.querySelector('#back-to-top');
+var downContact = document.querySelector("#back-to-top");
 
+var vcfStructure = `
+BEGIN:VCARD
+VERSION:3.0
+PRODID:-//Apple Inc.//iPhone OS 9.2.1//EN
+N:${LastName};${FirstName};;;
+FN: Zintech
+ORG: ${slogan};
+TEL;type=${typePhone};type=VOICE;type=pref:${phone}
+END:VCARD
+`; 
+
+var encodeVcf = btoa(vcfStructure);
 function downloadCSV() {
-    var vcf = "data:text/x-vcard;charset=utf-8;base64,QkVHSU46VkNBUkQNClZFUlNJT046My4wDQpQUk9ESUQ6LS8vQXBwbGUgSW5jLi8vaVBob25lIE9TIDkuMi4xLy9FTg0KTjpaaW50ZWNoO0NvbXBhbnk7OzsNCkZOOiBaaW50ZWNoDQpPUkc6IGxlYWQgeW91ciBzb2x1dGlvbjsNClRFTDt0eXBlPVdPUks7dHlwZT1WT0lDRTt0eXBlPXByZWY6MDg1ODYyNjc2OA0KRU5EOlZDQVJEDQo="; //Creates CSV File Format
-    var excel = encodeURI(vcf); //Links to vcf
-    var link = document.createElement("a");
-    link.setAttribute("href", excel); //Links to vcf File
-    link.setAttribute("download", "test.vcf"); //Filename that vcf is saved as
-    link.click();
+  var vcf = `data:text/x-vcard;charset=utf-8;base64,${encodeVcf}`; //Creates CSV File Format
+  var excel = encodeURI(vcf); //Links to vcf
+  var link = document.createElement("a");
+  link.setAttribute("href", excel); //Links to vcf File
+  link.setAttribute("download", "test.vcf"); //Filename that vcf is saved as
+  link.click();
 }
 
 downContact.addEventListener("click", downloadCSV);
-
